26fb15d2c095bf6c6aec9a8d4aa8500830c2b4e7
Anbiniyar Muniandy <anbin@codingrequired.com>
2025-07-31 16:01:16 -0400

    Fix the query status

--- src/articles/list-articles.ts
+++ src/articles/list-articles.ts
@@ -2,7 +2,7 @@ import {buildUrl, sendGetRequest} from "../http-utils";
 import {CollectionResponse} from "./collection-response";
 import {ArticleRef} from "./article-ref";
 
-export type ListArticlesVisibility = 'all' | 'public' | 'private';
+export type ListArticlesVisibility = 'all' | 'published' | 'notpublished';
 export type ListArticlesSort = 'number' | 'status' | 'name' | 'popularity'  | 'createdAt' | 'updatedAt';
 export type ListArticlesOrder = 'asc' | 'desc';
 
